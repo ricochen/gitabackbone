5a13849115eb5b2ec5b261a109525e9cd09ded28
Rico Chen <ricochenx@gmail.com>
2016-05-25 14:27:13 -0700

    [feat] add promise to render API data as HTML

--- app/request/request.js
+++ app/request/request.js
@@ -1,12 +1,23 @@
 angular.module('app.request', [])
 
-.controller('RequestController', function($scope, request, speech) {
+.controller('RequestController', function($scope, $sce, request, speech) {
   $scope.data = {};
 
   if (speech.checkRecognition()) {
     var recognition = speech.createRecognition();
     speech.speechToText(recognition, $scope.data, function(data) {
-      request.sendRequest(data);
+
+      request.sendRequest(data)
+        .then(function(APIdata) {
+          $scope.renderAPIdata = function() {
+            return $sce.trustAsHtml(APIdata.data);
+          };
+          console.log('Client side APIdata: ' + APIdata); // APIdata not logging to console correctly, needs fix
+        })
+        .catch(function (error) {
+          console.error('Rendering HTML for APIdata unsuccessful ' + error);
+        });
+
     });
   }
 
