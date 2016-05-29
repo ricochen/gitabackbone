4648fccc8a3da89b805a5f965b1a362926ef74d5
Rico Chen <ricochenx@gmail.com>
2016-05-29 14:44:38 -0700

    [refactor] move parseURL to seperate file

--- server/server.js
+++ server/server.js
@@ -2,17 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var axios = require('axios');
+var parseURL = require('./parseURL.js');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3468;
 
 app.use(express.static(__dirname + '/../app'));
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
-  var text = req.body.text.toLowerCase();
-  var url = parseURL(text);
-  console.log('text: ' + text);
-  console.log('url: ' + url);
+  var text = req.body.text.toLowerCase().split(' ');
+  var url = parseURL.parseURL(text);
 
   // External API get request from url
   axios.get(url)
@@ -20,25 +19,10 @@ app.post('/', function(req, res) {
       res.send(response.data); // Send data to client for rendering
     })
     .catch(function (response) {
-      console.log('GET request from ' + url + ' unsuccessful: ' + response);
+      console.log('GET request from ' + url + ' unsuccessful.' + response);
     });
 });
 
 app.listen(port, function () {
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port ' + port);
 });
-
-function parseURL(text) {
-  var url;
-  var word = text.split(' ').slice(1);
-
-  if (text.includes('google')) {
-    word = word.join(' ');
-    url = 'https://www.google.com/search?q=' + word;
-  } else if (text.includes('reddit')) {
-    word = word.join('');
-    url = 'https://www.reddit.com/r/' + word;
-  }
-
-  return url;
-}
