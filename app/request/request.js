ad5d6f380117ef865799ac4d38a3782a73c3d6ed
Rico Chen <ricochenx@gmail.com>
2016-05-30 16:05:43 -0700

    [feat] add recording intervals for user speech

--- app/request/request.js
+++ app/request/request.js
@@ -5,25 +5,29 @@ angular.module('app.request', [])
   $scope.home = 'Hi, I\'m kirk!';
   $scope.home2 = 'What can I do for you?';
   jQuery('#waves').show();
-  jQuery('body').toggleClass('bg');
+  jQuery('body').addClass('bg');
+  record();
+  setInterval(record, 5000);
 
-  if (speech.checkRecognition()) {
-    var recognition = speech.createRecognition();
-    speech.speechToText(recognition, $scope.data, function(data) {
+  function record() {
+    if (speech.checkRecognition()) {
+      var recognition = speech.createRecognition();
+      speech.speechToText(recognition, $scope.data, function(data) {
 
-      request.sendRequest(data)
-        .then(function(APIdata) {
-          $scope.home = '';
-          $scope.home2 = '';
-          jQuery('#waves').hide();
-          jQuery('body').toggleClass('bg');
-          $scope.APIdata = $sce.trustAsHtml(APIdata.data);
-        })
-        .catch(function (error) {
-          console.error('Rendering HTML for APIdata unsuccessful ' + error);
-        });
+        request.sendRequest(data)
+          .then(function(APIdata) {
+            $scope.home = '';
+            $scope.home2 = '';
+            jQuery('#waves').hide();
+            jQuery('body').removeClass('bg');
+            $scope.APIdata = $sce.trustAsHtml(APIdata.data);
+          })
+          .catch(function (error) {
+            console.error('Rendering HTML for APIdata unsuccessful ' + error);
+          });
 
-    });
+      });
+    }
   }
 
 });
